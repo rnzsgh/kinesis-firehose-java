24f872443ba2586961c9bef2326505226fcaca64
Ryan <ryannitz@amazon.com>
2018-01-04 16:49:00 -0500

    Dev

--- src/main/java/demo/KinesisFirehose.java
+++ src/main/java/demo/KinesisFirehose.java
@@ -28,8 +28,6 @@ package demo;
 import com.amazonaws.services.kinesisfirehose.model.PutRecordBatchResult;
 import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehoseAsync;
 import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehoseAsyncClientBuilder;
-import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehose;
-import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehoseClientBuilder;
 import com.amazonaws.services.kinesisfirehose.model.Record;
 import com.amazonaws.services.kinesisfirehose.model.PutRecordBatchRequest;
 import com.amazonaws.services.kinesisfirehose.model.PutRecordBatchResponseEntry;
@@ -37,40 +35,47 @@ import com.amazonaws.services.kinesisfirehose.model.PutRecordBatchResponseEntry;
 // Java
 import java.nio.ByteBuffer;
 import java.util.Iterator;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.stream.Collectors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
 
 public class KinesisFirehose {
 
   private static final int RECORD_COUNT = 100000000;
   private static final int RECORD_LENGTH = 1000;
-  private static final int RECORD_BATCH_COUNT = 10;
+  private static final int RECORD_BATCH_COUNT = 500;
   private static final long READ_TIMEOUT_MS = 250;
   private static final long BATCH_TIMEOUT_MS = 1000;
   private static final long BUFFER_TIMEOUT_MS = 250;
+  private static final int MAX_FUTURES = 5;
+  private static final int LOADER_THREADS = 12;
   private static final String FIREHOSE_STREAM_NAME = "test";
 
   public static void main(final String [] pArgs) throws Exception {
-    final ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(10000);
+    final ArrayBlockingQueue<Record> queue = new ArrayBlockingQueue<>(10000);
 
-    final CountDownLatch countDownLatch = new CountDownLatch(1);
+    final CountDownLatch countDownLatch = new CountDownLatch(LOADER_THREADS);
 
     final AtomicBoolean running = new AtomicBoolean(true);
 
-    new Loader(
-        queue,
-        countDownLatch,
-        READ_TIMEOUT_MS,
-        BATCH_TIMEOUT_MS,
-        RECORD_BATCH_COUNT,
-        FIREHOSE_STREAM_NAME,
-        running
-    ).start();
+    for (int idx=0; idx < LOADER_THREADS; idx++) {
+      new Loader(
+          queue,
+          countDownLatch,
+          READ_TIMEOUT_MS,
+          BATCH_TIMEOUT_MS,
+          RECORD_BATCH_COUNT,
+          MAX_FUTURES,
+          FIREHOSE_STREAM_NAME,
+          running
+      ).start();
+    }
 
     new Producer(queue, RECORD_COUNT, RECORD_LENGTH, running).start();
 
@@ -79,12 +84,12 @@ public class KinesisFirehose {
 
   private static class Producer extends Thread {
 
-    private final ArrayBlockingQueue<String> _queue;
+    private final ArrayBlockingQueue<Record> _queue;
     private final int _recordCount;
     private final int _recordLength;
     private final AtomicBoolean _running;
 
-    public Producer(final ArrayBlockingQueue<String> pQueue,
+    public Producer(final ArrayBlockingQueue<Record> pQueue,
                     final int pRecordCount,
                     final int pRecordLength,
                     final AtomicBoolean pRunning)
@@ -100,7 +105,7 @@ public class KinesisFirehose {
       try {
         for (int idx=0; idx < _recordCount; idx++) {
           // Blocking call if queue is full
-          _queue.put(testData);
+          _queue.put(new Record().withData(ByteBuffer.wrap(testData.getBytes("UTF-8"))));
         }
 
         // Tell the loader that we are done
@@ -112,23 +117,25 @@ public class KinesisFirehose {
 
   private static class Loader extends Thread {
 
-    private final ArrayBlockingQueue<String> _queue;
+    private final ArrayBlockingQueue<Record> _queue;
     private final CountDownLatch _countDownLatch;
     private final long _readTimeout;
     private final long _batchTimeout;
     private final int _batchSize;
     private final String _streamName;
     private final AtomicBoolean _running;
+    private final int _maxFutures;
 
     // Assumes you are using an IAM role for credentials or have ~/.aws/credentials in place
     //private final AmazonKinesisFirehose _firehoseClient = AmazonKinesisFirehoseClientBuilder.defaultClient();
     private final AmazonKinesisFirehoseAsync _firehoseClient = AmazonKinesisFirehoseAsyncClientBuilder.defaultClient();
 
-    public Loader(final ArrayBlockingQueue<String> pQueue,
+    public Loader(final ArrayBlockingQueue<Record> pQueue,
                   final CountDownLatch pCountDownLatch,
                   final long pReadTimeout,
                   final long pBatchTimeout,
                   final int pBatchSize,
+                  final int pMaxFutures,
                   final String pStreamName,
                   final AtomicBoolean pRunning)
     {
@@ -137,11 +144,13 @@ public class KinesisFirehose {
       _readTimeout = pReadTimeout;
       _batchTimeout = pBatchTimeout;
       _batchSize = pBatchSize;
+      _maxFutures = pMaxFutures;
       _streamName = pStreamName;
       _running = pRunning;
     }
 
-    private boolean checkFutures(final ArrayList<Future<PutRecordBatchResult>> pFutures) throws Exception {
+    /*
+    private boolean checkFutures(final LinkedList<Future<PutRecordBatchResult>> pFutures) throws Exception {
       boolean same = true;
       for (Iterator<Future<PutRecordBatchResult>> iter = pFutures.iterator(); iter.hasNext();) {
         final Future<PutRecordBatchResult> future = iter.next();
@@ -163,61 +172,69 @@ public class KinesisFirehose {
 
       return same;
     }
+    */
+
+    private void addRecordsToQueue(final LinkedList<Record> pRecords) {
+      pRecords.forEach(r -> {
+        try { _queue.put(r); } catch (final Throwable t) { throw new IllegalStateException(t); }
+      });
+    }
 
     @Override public void run() {
       try {
-        String record = null;
-
-        final ArrayList<Record> records = new ArrayList<>();
+        final LinkedList<Record> records = new LinkedList<>();
 
         long lastFlush = System.currentTimeMillis();
-
-        final ArrayList<Future<PutRecordBatchResult>> futures = new ArrayList<>();
 
         long flushCount = 0;
 
         while (_running.get()) {
 
-          if (futures.size() >= 500) {
-            if (checkFutures(futures)) {
-              sleep(_readTimeout);
-              continue;
-            }
-          }
-
-          record = _queue.poll(_readTimeout, TimeUnit.MILLISECONDS);
+          final Record record = _queue.poll(_readTimeout, TimeUnit.MILLISECONDS);
 
           if (record != null) {
-            records.add(new Record().withData(ByteBuffer.wrap(record.getBytes("UTF-8"))));
+            records.add(record);
           }
 
-          if (records.size() >= _batchSize || System.currentTimeMillis() - lastFlush > _batchTimeout) {
+          if (records.size() >= _batchSize || ((System.currentTimeMillis() - lastFlush > _batchTimeout) && records.size() > 0)) {
             flushCount++;
             System.out.println("Flushing: " + flushCount);
 
             // Flush
             final PutRecordBatchRequest request = new PutRecordBatchRequest();
             request.setDeliveryStreamName(_streamName);
-            request.setRecords(records.stream().collect(Collectors.toList()));
+            request.setRecords(records);
+
+            try {
+
+              final PutRecordBatchResult result  = _firehoseClient.putRecordBatchAsync(request).get();
+
+              if (result.getFailedPutCount() > 0) {
+                System.out.println("failed count: " + result.getFailedPutCount());
+              }
+
+              int idx = 0;
+              for (final PutRecordBatchResponseEntry entry : result.getRequestResponses()) {
+
+                // TODO: Check error codes
+                if (entry.getErrorCode() != null) {
+                  _queue.put(records.get(idx));
+                }
+                idx++;
+              }
+
+              lastFlush = System.currentTimeMillis();
+
+            } catch (final CancellationException ce) {
+              addRecordsToQueue(records);
+              ce.printStackTrace();
+            } catch (final ExecutionException ee) {
+              addRecordsToQueue(records);
+              ee.printStackTrace();
+            }
 
             records.clear();
-            lastFlush = System.currentTimeMillis();
-
-            /*
-            request.setRecords(records);
-            final PutRecordBatchResult result = _firehoseClient.putRecordBatch(request);
-            if (result.getFailedPutCount() > 0) {
-              System.out.println("Failed put count: " + result.getFailedPutCount());
-              // TODO: Handle error conditions - loop through the  getRequestResponses()
-            }
-            */
-
-            futures.add(_firehoseClient.putRecordBatchAsync(request));
-            checkFutures(futures);
-
           }
-
-          // TODO: Handle errors
         }
 
         // TODO: Make sure the queue is empty
