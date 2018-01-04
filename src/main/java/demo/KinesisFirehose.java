72d08d23cd6d9593d872bc66b347bd4d2a493009
Ryan <ryannitz@amazon.com>
2018-01-04 17:30:53 -0500

    Dev

--- src/main/java/demo/KinesisFirehose.java
+++ src/main/java/demo/KinesisFirehose.java
@@ -15,11 +15,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Bastion stack creation prerequisite:  first create an EC2 key pair and a VPC stack.
- * For details about how to connect to a Linux instance in a private subnet via the
- * bastion, see the following AWS blog post:
- * https://aws.amazon.com/blogs/security/securely-connect-to-linux-instances-running-in-a-private-amazon-vpc/
  */
 
 package demo;
@@ -33,84 +28,64 @@ import com.amazonaws.services.kinesisfirehose.model.PutRecordBatchRequest;
 import com.amazonaws.services.kinesisfirehose.model.PutRecordBatchResponseEntry;
 
 // Java
+import java.util.Timer;
+import java.util.TimerTask;
 import java.nio.ByteBuffer;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.stream.Collectors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 
 public class KinesisFirehose {
 
-  private static final int RECORD_COUNT = 100000000;
+  private static final int MAX_QUEUE_SIZE = 30000;
   private static final int RECORD_LENGTH = 1000;
   private static final int RECORD_BATCH_COUNT = 500;
   private static final long READ_TIMEOUT_MS = 250;
   private static final long BATCH_TIMEOUT_MS = 1000;
   private static final long BUFFER_TIMEOUT_MS = 250;
-  private static final int MAX_FUTURES = 5;
   private static final int LOADER_THREADS = 12;
+  private static final int REQUESTS_PER_SECOND = 5000;
   private static final String FIREHOSE_STREAM_NAME = "test";
 
   public static void main(final String [] pArgs) throws Exception {
-    final LinkedBlockingQueue<Record> queue = new LinkedBlockingQueue<>();
-
-    final CountDownLatch countDownLatch = new CountDownLatch(LOADER_THREADS);
-
-    final AtomicBoolean running = new AtomicBoolean(true);
+    final LinkedBlockingQueue<Record> queue = new LinkedBlockingQueue<>(MAX_QUEUE_SIZE);
 
     for (int idx=0; idx < LOADER_THREADS; idx++) {
       new Loader(
           queue,
-          countDownLatch,
           READ_TIMEOUT_MS,
           BATCH_TIMEOUT_MS,
           RECORD_BATCH_COUNT,
-          MAX_FUTURES,
-          FIREHOSE_STREAM_NAME,
-          running
+          FIREHOSE_STREAM_NAME
       ).start();
     }
 
-    new Producer(queue, RECORD_COUNT, RECORD_LENGTH, running).start();
+    for (int idx=0; idx < (REQUESTS_PER_SECOND * 2) / 1000; idx++) {
+      new Producer(queue, RECORD_LENGTH);
+    }
 
-    countDownLatch.await();
+    Thread.sleep(Integer.MAX_VALUE);
   }
 
-  private static class Producer extends Thread {
+  private static class Producer extends TimerTask {
 
     private final LinkedBlockingQueue<Record> _queue;
-    private final int _recordCount;
-    private final int _recordLength;
-    private final AtomicBoolean _running;
+    private final Timer _timer = new Timer(true);
+    private final byte[] _testData;
 
-    public Producer(final LinkedBlockingQueue<Record> pQueue,
-                    final int pRecordCount,
-                    final int pRecordLength,
-                    final AtomicBoolean pRunning)
-    {
+    public Producer(final LinkedBlockingQueue<Record> pQueue, final int pRecordLength) throws Exception {
       _queue = pQueue;
-      _recordCount = pRecordCount;
-      _recordLength = pRecordLength;
-      _running = pRunning;
+      _testData = new String(new byte[pRecordLength]).getBytes("UTF-8");
+      _timer.scheduleAtFixedRate(this, 0, 1);
     }
 
     @Override public void run() {
-      final String testData = new String(new byte[_recordLength]);
-      try {
-        for (int idx=0; idx < _recordCount; idx++) {
-          // Blocking call if queue is full
-          _queue.put(new Record().withData(ByteBuffer.wrap(testData.getBytes("UTF-8"))));
-        }
-
-        // Tell the loader that we are done
-        _running.set(false);
-        System.out.println("Producer is done");
+      try { _queue.put(new Record().withData(ByteBuffer.wrap(_testData)));
       } catch (final Throwable t) { throw new IllegalStateException(t); }
     }
   }
@@ -118,61 +93,26 @@ public class KinesisFirehose {
   private static class Loader extends Thread {
 
     private final LinkedBlockingQueue<Record> _queue;
-    private final CountDownLatch _countDownLatch;
     private final long _readTimeout;
     private final long _batchTimeout;
     private final int _batchSize;
     private final String _streamName;
-    private final AtomicBoolean _running;
-    private final int _maxFutures;
 
     // Assumes you are using an IAM role for credentials or have ~/.aws/credentials in place
-    //private final AmazonKinesisFirehose _firehoseClient = AmazonKinesisFirehoseClientBuilder.defaultClient();
     private final AmazonKinesisFirehoseAsync _firehoseClient = AmazonKinesisFirehoseAsyncClientBuilder.defaultClient();
 
     public Loader(final LinkedBlockingQueue<Record> pQueue,
-                  final CountDownLatch pCountDownLatch,
                   final long pReadTimeout,
                   final long pBatchTimeout,
                   final int pBatchSize,
-                  final int pMaxFutures,
-                  final String pStreamName,
-                  final AtomicBoolean pRunning)
+                  final String pStreamName)
     {
       _queue = pQueue;
-      _countDownLatch = pCountDownLatch;
       _readTimeout = pReadTimeout;
       _batchTimeout = pBatchTimeout;
       _batchSize = pBatchSize;
-      _maxFutures = pMaxFutures;
       _streamName = pStreamName;
-      _running = pRunning;
     }
-
-    /*
-    private boolean checkFutures(final LinkedList<Future<PutRecordBatchResult>> pFutures) throws Exception {
-      boolean same = true;
-      for (Iterator<Future<PutRecordBatchResult>> iter = pFutures.iterator(); iter.hasNext();) {
-        final Future<PutRecordBatchResult> future = iter.next();
-        if (future.isDone()) {
-          final PutRecordBatchResult result = future.get();
-
-          if (result.getFailedPutCount() > 0) {
-            System.out.println("Failed count: " + result.getFailedPutCount());
-            // TODO: Handle error conditions - loop through the  getRequestResponses()
-            // and process the failed records. This requires caching the records in memory
-            // until there is a confirmation that and then removing it
-          }
-
-          iter.remove();
-          same = false;
-          System.out.println("remove done - size: " + pFutures.size());
-        }
-      }
-
-      return same;
-    }
-    */
 
     private void addRecordsToQueue(final LinkedList<Record> pRecords) {
       pRecords.forEach(r -> {
@@ -188,7 +128,7 @@ public class KinesisFirehose {
 
         long flushCount = 0;
 
-        while (_running.get()) {
+        while (true) {
 
           final Record record = _queue.poll(_readTimeout, TimeUnit.MILLISECONDS);
 
@@ -236,12 +176,6 @@ public class KinesisFirehose {
             records.clear();
           }
         }
-
-        // TODO: Make sure the queue is empty
-
-        System.out.println("Loader is done");
-        _countDownLatch.countDown();
-
       } catch (final Throwable t) { throw new IllegalStateException(t); }
     }
   }
