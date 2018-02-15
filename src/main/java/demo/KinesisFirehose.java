590362d3d07844590d71743a9cf1fee93b4c1c66
Ryan <ryannitz@amazon.com>
2018-02-15 14:37:18 -0500

    Dev

--- src/main/java/demo/KinesisFirehose.java
+++ src/main/java/demo/KinesisFirehose.java
@@ -27,6 +27,9 @@ import com.amazonaws.services.kinesisfirehose.model.Record;
 import com.amazonaws.services.kinesisfirehose.model.PutRecordBatchRequest;
 import com.amazonaws.services.kinesisfirehose.model.PutRecordBatchResponseEntry;
 
+// Guava
+import com.google.common.util.concurrent.RateLimiter;
+
 // Java
 import java.util.Timer;
 import java.util.TimerTask;
@@ -46,11 +49,15 @@ public class KinesisFirehose {
   private static final long READ_TIMEOUT_MS = 250;
   private static final long BATCH_TIMEOUT_MS = 2000;
   private static final int LOADER_THREADS = 15;
-  private static final int REQUESTS_PER_SECOND = 7000;
+  //private static final int REQUESTS_PER_SECOND = 7000;
+  private static final int RECORDS_PER_SECOND = 50000;
+
   private static final String FIREHOSE_STREAM_NAME = "test";
 
   public static void main(final String [] pArgs) throws Exception {
     final LinkedBlockingQueue<Record> queue = new LinkedBlockingQueue<>(MAX_QUEUE_SIZE);
+
+    final RateLimiter rateLimiter = RateLimiter.create(RECORDS_PER_SECOND);
 
     // Assumes you are using an IAM role for credentials or have ~/.aws/credentials in place
     final AmazonKinesisFirehose firehoseClient = AmazonKinesisFirehoseClientBuilder.defaultClient();
@@ -59,6 +66,7 @@ public class KinesisFirehose {
       new Loader(
           queue,
           firehoseClient,
+          rateLimiter,
           READ_TIMEOUT_MS,
           BATCH_TIMEOUT_MS,
           RECORD_BATCH_COUNT,
@@ -66,27 +74,32 @@ public class KinesisFirehose {
       ).start();
     }
 
-    for (int idx=0; idx < REQUESTS_PER_SECOND / 1000; idx++) {
-      new Producer(queue, RECORD_LENGTH);
-    }
+    //for (int idx=0; idx < REQUESTS_PER_SECOND / 1000; idx++) {
+    new Producer(queue, RECORD_LENGTH);
+    //}
 
     Thread.sleep(Integer.MAX_VALUE);
   }
 
-  private static class Producer extends TimerTask {
+  private static class Producer extends Thread {
 
     private final LinkedBlockingQueue<Record> _queue;
-    private final Timer _timer = new Timer(true);
+    //private final Timer _timer = new Timer(true);
     private final byte[] _testData;
 
     public Producer(final LinkedBlockingQueue<Record> pQueue, final int pRecordLength) throws Exception {
       _queue = pQueue;
       _testData = new String(new byte[pRecordLength]).getBytes("UTF-8");
-      _timer.scheduleAtFixedRate(this, 500, 1);
+      //_timer.scheduleAtFixedRate(this, 500, 1);
     }
 
     @Override public void run() {
-      try { _queue.put(new Record().withData(ByteBuffer.wrap(_testData)));
+      try {
+
+        while (true) {
+          _queue.put(new Record().withData(ByteBuffer.wrap(_testData)));
+        }
+
       } catch (final Throwable t) { throw new IllegalStateException(t); }
     }
   }
@@ -99,9 +112,11 @@ public class KinesisFirehose {
     private final long _batchTimeout;
     private final int _batchSize;
     private final String _streamName;
+    private final RateLimiter _rateLimiter;
 
     public Loader(final LinkedBlockingQueue<Record> pQueue,
                   final AmazonKinesisFirehose pFirehoseClient,
+                  final RateLimiter pRateLimiter,
                   final long pReadTimeout,
                   final long pBatchTimeout,
                   final int pBatchSize,
@@ -109,6 +124,7 @@ public class KinesisFirehose {
     {
       _queue = pQueue;
       _firehoseClient = pFirehoseClient;
+      _rateLimiter = pRateLimiter;
       _readTimeout = pReadTimeout;
       _batchTimeout = pBatchTimeout;
       _batchSize = pBatchSize;
@@ -118,6 +134,8 @@ public class KinesisFirehose {
     private void sendRecords(final LinkedList<Record> pRecords) throws Exception {
 
       try {
+
+        _rateLimiter.acquire(pRecords.size());
 
         final LinkedList<Record> failed = new LinkedList<>();
 
